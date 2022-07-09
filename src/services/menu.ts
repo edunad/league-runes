d8bc1cd63a95875f8548aefbf562493aa7b094c6
FailCake <edunad@users.noreply.github.com>
2022-07-09 12:52:52 +0200

    Fix inquirer rendering

--- src/services/menu.ts
+++ src/services/menu.ts
@@ -8,7 +8,7 @@ import { Role } from '../types/role';
 import { SettingsService } from './settings';
 
 export class MenuService {
-    private static readonly MAX_LOG: number = 6;
+    private static readonly MAX_LOG: number = 7;
 
     private static championAvatar: string;
     private static logger: string[];
@@ -36,19 +36,28 @@ export class MenuService {
         this.printLog();
     }
 
-    public static clear(): void {
+    public static async clear(): Promise<void> {
         console.clear();
-        this.printLog();
+        await this.printLog();
     }
 
-    private static printLog(): void {
-        process.stdout.cursorTo(0, 10);
-        process.stdout.clearScreenDown();
-        this.logger.forEach((log, indx) => {
-            if (indx >= this.MAX_LOG) return;
-            console.log(log);
+    private static printLog(): Promise<void> {
+        return new Promise<void>((res) => {
+            process.stdout.cursorTo(0, 10, () => {
+                process.stdout.clearScreenDown(() => {
+                    this.logger.forEach((log, indx) => {
+                        if (indx > this.MAX_LOG) return;
+                        console.log(log);
+                    });
+
+                    process.stdout.cursorTo(0, 0, () => {
+                        if (this.mainmenu) global.__forceCheckboxRender();
+                        if (this.roleMenu) global.__forceListRender();
+                        res();
+                    });
+                });
+            });
         });
-        process.stdout.cursorTo(0, 0);
     }
 
     public static setChampionAvatar(champion: string): void {
@@ -78,51 +87,52 @@ export class MenuService {
         if (this.roleMenu) return; // Already opened?
 
         this.killMenus();
-        this.clear();
+        this.clear().then(() => {
+            process.stdout.cursorTo(0, 0, () => {
+                this.roleMenu = inquirer.prompt([
+                    {
+                        type: 'list',
+                        message: `INGAME - OVERRIDE ROLE`,
+                        name: 'role',
+                        choices: [
+                            {
+                                name: 'DEFAULT - META',
+                                value: null,
+                            },
+                            new inquirer.Separator(' ===='),
+                            {
+                                name: 'JUNGLE',
+                                value: 'jungle',
+                            },
+                            {
+                                name: 'TOP',
+                                value: 'top',
+                            },
+                            {
+                                name: 'MIDDLE',
+                                value: 'middle',
+                            },
+                            {
+                                name: 'BOTTOM (ADC)',
+                                value: 'bottom',
+                            },
+                            {
+                                name: 'SUPPORT',
+                                value: 'support',
+                            },
+                        ],
+                    },
+                ]);
 
-        console.log(`------------------ CHAMPION SELECT ------------------`);
-        this.roleMenu = inquirer.prompt([
-            {
-                type: 'list',
-                message: `Override role`,
-                name: 'role',
-                choices: [
-                    {
-                        name: 'DEFAULT - META',
-                        value: null,
-                    },
-                    new inquirer.Separator(' ===='),
-                    {
-                        name: 'JUNGLE',
-                        value: 'jungle',
-                    },
-                    {
-                        name: 'TOP',
-                        value: 'top',
-                    },
-                    {
-                        name: 'MIDDLE',
-                        value: 'middle',
-                    },
-                    {
-                        name: 'BOTTOM (ADC)',
-                        value: 'bottom',
-                    },
-                    {
-                        name: 'SUPPORT',
-                        value: 'support',
-                    },
-                ],
-            },
-        ]);
+                this.roleMenu.then((data) => {
+                    if (!data) return;
 
-        this.roleMenu.then((data) => {
-            if (!data) return;
+                    this.event.emit('onRoleChange', data.role as Role);
+                    this.roleMenu = null;
 
-            this.event.emit('onRoleChange', data.role as Role);
-            this.roleMenu = null;
-            process.stdout.cursorTo(0, 2);
-            this.printRole();
+                    this.printRole();
+                });
+            });
         });
     }
 
@@ -130,77 +140,78 @@ export class MenuService {
         if (this.mainmenu) return; // Already opened?
 
         this.killMenus();
-        this.clear();
+        this.clear().then(() => {
+            process.stdout.cursorTo(0, 0, () => {
+                this.mainmenu = inquirer.prompt([
+                    {
+                        type: 'checkbox',
+                        message: 'MAINMENU -- SETTINGS',
+                        name: 'settings',
+                        choices: [
+                            new inquirer.Separator('SKINS ===='),
+                            {
+                                name: '  Enable random skin',
+                                value: 'skin-enabled',
+                                checked: SettingsService.getSetting('skin-enabled'),
+                            },
+                            {
+                                name: '  Enable random chroma skin',
+                                value: 'skin-chroma-enabled',
+                                checked: SettingsService.getSetting('skin-chroma-enabled'),
+                            },
+                            new inquirer.Separator('AUTO ===='),
+                            {
+                                name: '  Enable Auto-Runes',
+                                value: 'autorunes-enabled',
+                                checked: SettingsService.getSetting('autorunes-enabled'),
+                            },
+                            {
+                                name: '  Enable Auto-Items',
+                                value: 'autoitems-enabled',
+                                checked: SettingsService.getSetting('autoitems-enabled'),
+                            },
+                        ],
+                        onEvent: (key, data) => {
+                            SettingsService.setSetting('skin-enabled', data.indexOf('skin-enabled') !== -1, false);
+                            SettingsService.setSetting('skin-chroma-enabled', data.indexOf('skin-chroma-enabled') !== -1, false);
+                            SettingsService.setSetting('autorunes-enabled', data.indexOf('autorunes-enabled') !== -1, false);
+                            SettingsService.setSetting('autoitems-enabled', data.indexOf('autoitems-enabled') !== -1, true);
+                        },
+                    },
+                    {
+                        type: 'list',
+                        message: `Providers - Picked: ${SettingsService.getSetting('provider')}`,
+                        name: 'providers',
+                        choices: [
+                            {
+                                name: 'U.GG',
+                                value: 'ugg',
+                            },
+                            {
+                                name: 'MetaSRC',
+                                value: 'metasrc',
+                            },
+                            {
+                                name: 'OP.GG (Bit broken / not recommented :S)',
+                                value: 'opgg',
+                            },
+                        ],
+                    },
+                ]);
 
-        console.log(`--------------------- MAIN MENU ---------------------`);
-        this.mainmenu = inquirer.prompt([
-            {
-                type: 'checkbox',
-                message: 'Settings',
-                name: 'settings',
-                choices: [
-                    new inquirer.Separator('SKINS ===='),
-                    {
-                        name: '  Enable random skin',
-                        value: 'skin-enabled',
-                        checked: SettingsService.getSetting('skin-enabled'),
-                    },
-                    {
-                        name: '  Enable random chroma skin',
-                        value: 'skin-chroma-enabled',
-                        checked: SettingsService.getSetting('skin-chroma-enabled'),
-                    },
-                    new inquirer.Separator('AUTO ===='),
-                    {
-                        name: '  Enable Auto-Runes',
-                        value: 'autorunes-enabled',
-                        checked: SettingsService.getSetting('autorunes-enabled'),
-                    },
-                    {
-                        name: '  Enable Auto-Items',
-                        value: 'autoitems-enabled',
-                        checked: SettingsService.getSetting('autoitems-enabled'),
-                    },
-                ],
-                onEvent: (key, data) => {
-                    SettingsService.setSetting('skin-enabled', data.indexOf('skin-enabled') !== -1, false);
-                    SettingsService.setSetting('skin-chroma-enabled', data.indexOf('skin-chroma-enabled') !== -1, false);
-                    SettingsService.setSetting('autorunes-enabled', data.indexOf('autorunes-enabled') !== -1, false);
-                    SettingsService.setSetting('autoitems-enabled', data.indexOf('autoitems-enabled') !== -1, true);
-                },
-            },
-            {
-                type: 'list',
-                message: `Providers - Picked: ${SettingsService.getSetting('provider')}`,
-                name: 'providers',
-                choices: [
-                    {
-                        name: 'U.GG',
-                        value: 'ugg',
-                    },
-                    {
-                        name: 'MetaSRC',
-                        value: 'metasrc',
-                    },
-                    {
-                        name: 'OP.GG (Bit broken / not recommented :S)',
-                        value: 'opgg',
-                    },
-                ],
-            },
-        ]);
+                this.mainmenu.then((data) => {
+                    if (!data) return;
 
-        this.mainmenu.then((data) => {
-            if (!data) return;
+                    SettingsService.setSetting('skin-enabled', data.settings.indexOf('skin-enabled') !== -1, false);
+                    SettingsService.setSetting('skin-chroma-enabled', data.settings.indexOf('skin-chroma-enabled') !== -1, false);
+                    SettingsService.setSetting('autorunes-enabled', data.settings.indexOf('autorunes-enabled') !== -1, false);
+                    SettingsService.setSetting('autoitems-enabled', data.settings.indexOf('autoitems-enabled') !== -1, false);
 
-            SettingsService.setSetting('skin-enabled', data.settings.indexOf('skin-enabled') !== -1, false);
-            SettingsService.setSetting('skin-chroma-enabled', data.settings.indexOf('skin-chroma-enabled') !== -1, false);
-            SettingsService.setSetting('autorunes-enabled', data.settings.indexOf('autorunes-enabled') !== -1, false);
-            SettingsService.setSetting('autoitems-enabled', data.settings.indexOf('autoitems-enabled') !== -1, false);
+                    SettingsService.setSetting('provider', data.providers);
 
-            SettingsService.setSetting('provider', data.providers);
-
-            this.mainmenu = null;
+                    this.mainmenu = null;
+                });
+            });
         });
     }
 }
